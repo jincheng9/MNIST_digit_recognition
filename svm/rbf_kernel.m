63ae4f649b0c6dd51511bc5f9a22052aed9ed2ef
Jincheng9 <perfume0607@gmail.com>
2014-03-18 16:09:45 +0800

    add radial basis kernel

---
+++ svm/rbf_kernel.m
@@ -0,0 +1,13 @@
+% load training set and testing set
+clear all;
+train_set = loadMNISTImages('train-images.idx3-ubyte')';
+train_label = loadMNISTLabels('train-labels.idx1-ubyte');
+test_set = loadMNISTImages('t10k-images.idx3-ubyte')';
+test_label = loadMNISTLabels('t10k-labels.idx1-ubyte');
+
+% training 
+model = svmtrain(train_label, train_set, '-s 0 -t 2 -g 0.5');
+
+% classification
+[predicted_label, accuracy, decision_values]=svmpredict(test_label, test_set, model);
+
