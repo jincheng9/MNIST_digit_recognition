93a85dd77437ee60929c67421e4eb329d92e8382
Jincheng9 <perfume0607@gmail.com>
2014-05-08 14:12:41 +0800

    update svm

--- svm/poly2_kernel.m
+++ svm/poly2_kernel.m
@@ -5,9 +5,14 @@ train_label = loadMNISTLabels('train-labels.idx1-ubyte');
 test_set = loadMNISTImages('t10k-images.idx3-ubyte')';
 test_label = loadMNISTLabels('t10k-labels.idx1-ubyte');
 
+
 % training 
-model = svmtrain(train_label, train_set, '-s 0 -t 1 -d 2');
-
+tic;
+model = svmtrain(train_label, train_set, '-s 0 -t 1 -d 2 -g 1 -r 1');
+t1 = toc;
 % classification
+tic;
 [predicted_label, accuracy, decision_values]=svmpredict(test_label, test_set, model);
-
+t2 = toc;
+disp(num2str(t1));
+disp(num2str(t2));
--- svm/rbf_kernel.m
+++ svm/rbf_kernel.m
@@ -5,9 +5,14 @@ train_label = loadMNISTLabels('train-labels.idx1-ubyte');
 test_set = loadMNISTImages('t10k-images.idx3-ubyte')';
 test_label = loadMNISTLabels('t10k-labels.idx1-ubyte');
 
-% training 
+% training
+tic; 
 model = svmtrain(train_label, train_set, '-s 0 -t 2');
-
+t1 = toc;
 % classification
+tic;
 [predicted_label, accuracy, decision_values]=svmpredict(test_label, test_set, model);
+t2 = toc;
+disp(num2str(t1));
+disp(num2str(t2));
 
